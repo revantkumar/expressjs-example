35686da8884304b6441d90b5f6fa70bd561fe419
Revant Kumar <revant12@gmail.com>
2015-10-31 15:24:31 -0400

    Adding User Service

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 
-// Include the Todo-lists service and let it bind to the application
-var todoService = require('./services/todo.js');
-todoService.LinkApp(app);
+// Include the User service and let it bind to the application
+var userService = require('./services/user.js');
+userService.LinkApp(app);
 
 var server = app.listen(8080, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log('Direct your browser to http://%s:%s/api/todo-lists', host, port);
+	console.log('Direct your browser to http://%s:%s/api/users', host, port);
 });
